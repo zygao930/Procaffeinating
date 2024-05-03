2e0903a483d144b12bd14ec386703a821cf51a9d
zygao930 <gaoziyuan2020@163.com>
2024-05-03 16:15:05 +0800

    add search bar etc

---
+++ lib/models/coffee.dart
@@ -0,0 +1,9 @@
+class Coffee{
+  final String name;
+  final String price;
+  final String imagePath;
+  final String description;
+
+
+
+}
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,6 +10,68 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('shop'),);
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Coffee o\'clock',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              )
+            ],
+          ),
+        ),
+        
+        //padding
+        const Padding(
+            padding: EdgeInsets.symmetric(vertical: 25.0),
+        ),
+
+        // hot drinks
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Text(
+                "Hot Drinks 🔥☕🔥",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                'See all',
+                style:
+                TextStyle(fontWeight: FontWeight.bold, color: Colors.brown[700]),
+              )
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        Expanded(
+          child: ListView.builder(
+              itemBuilder: (context, index){
+                return CoffeeTile();
+              }
+          ),
+        )
+      ],
+    );
   }
 }
